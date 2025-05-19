a0ea94f2ab8f023d0c559d50091d5094a06b1c36
jzongyun <jeong.yun@tukorea.ac.kr>
2025-05-19 22:35:16 +0900

    fix: header 경로 수정

--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import MobileHeader from "@/components/headers/MobileHeader";
+import MobileHeader from "@/components/header/MobileHeader";
 
 const MobileLayout : React.FC = () => {
   return (
